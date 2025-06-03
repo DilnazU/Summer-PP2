d5971dfcdc2ba3c3b70073c6ac22d4fc8e6fc73e
DilnazU <145113300+DilnazU@users.noreply.github.com>
2025-06-03 16:13:12 +0600

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,153 @@
+if 5 > 2:
+     print("Five is greater than two!")
+if 5 > 2:
+     print("Five is greater than two!") 
+     print("Five is greater than two!")
+#This is a comment.
+#kdoa
+#jndc
+print("PP2") #This is a comment.
+#This is a comment.
+"""
+samcjn
+"""
+x=23
+y="summer"
+print(x)
+print(y)
+a=23
+a="pp"
+print(a)
+x = str(23)    
+y = int(23)    
+z = float(23)
+print(x,y,z)
+x = 23
+y = "PP2"
+print(type(x))
+print(type(y))
+x="PP2"
+x='PP2'
+print(x)
+a = 23
+A = "PP2"
+print(A,a)
+SummerSchool="PP2"
+summerSchool="PP2"
+Summer_School="PP2"
+print(summerSchool,Summer_School,SummerSchool)
+x, y, z = "Programming", "Principles", "Python"
+print(x)
+print(y)
+print(z)
+x = y = z = "PP2"
+print(x)
+print(y)
+print(z)
+SummerSchool = ["Programming ", "Principles ", "(Python) "]
+x, y, z = SummerSchool
+print(x)
+print(y)
+print(z)
+print(x+y+z)
+x=23
+y=23
+print(x+y)
+x = "aa"
+
+def myfunc():
+  x = "bb"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+x = "af"
+
+def myfunc():
+  global x
+  x = "bb"
+
+myfunc()
+
+print("Python is " + x)
+x= 23
+y= 23.0
+z= 23j
+w=-23
+u=23E4
+t=23e4
+s=-23e4
+r=5+23j
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+print(type(u))
+print(type(s)) 
+print(type(t))
+print(type(r))
+
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+
+print(random.randrange(1, 23))
+
+x = int(23)   
+y = int(23.8) 
+z = int("23")
+w = float (23)
+u = str (23.4)
+print(u,w,x,y,z)
+a =  """Seasons 
+        Autumn is yellow,
+        Winter is white, 
+        Spring is green,
+        Summer is bright! ."""
+print(a)
+
+a = "PP2"
+print(a[1])
+
+for x in "python":
+  print(x)
+
+a = "PP2"
+print(len(a))
+
+txt = "Python is language of Programming Principles 2 "
+print("of" in txt)
+if "of" in txt:
+  print("Yes")
+txt = "Python is language of Programming Principles 2 "
+print("C++" not in txt)
+if "C++"in txt:
+  print("No")
+
+b = "Programming"
+print(b[2:4])
+print(b[:5])
+print(b[2:])
+print(b[-5:-2])
+print(b.upper())
+print(b.lower())
+print(b.replace("g", "i"))
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+age = 23
+txt = f" I am {age}"
+print(txt)
+txt = f"The price is {23 * 23} dollars"
+print(txt)
+txt = "\"PP2\""
